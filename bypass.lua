e7978310ed6be6ea34c58972ab14c2656bd157e8
Aqua Tsukimura <165631517+AquaTsukimura@users.noreply.github.com>
2026-05-03 17:00:04 +0500

    Update bypass.lua

--- bypass.lua
+++ bypass.lua
@@ -11,3 +11,20 @@ if not __int64_t then
 end
 
 local EntityPlayer = (__int64_t*(MEM[0x8333EF]))
+if not EntityPlayer then
+  EntityPlayer = __int32_t*({})
+end
+
+function bypass()
+  togBypass = not togBypass
+end
+
+function EntityPlayer:onUpdate(thiz)
+  thiz = __int64_t*(MEM[0x17EF382])
+  if thiz then
+    thiz = MEM[0x348EF8](thiz)
+    if thiz then
+      thiz.onGround = togBypass
+    end
+  end
+end
